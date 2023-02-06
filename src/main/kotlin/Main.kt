2e98fde63348ac44a03c78c452e9b64268bddf55
Angeth-Herjok <angethherjok@gmail.com>
2023-02-06 14:39:51 +0300

    Tanzania

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    var name ="Angeth"
+    var age ="90"
+    var weight ="160kg"
+    var height ="5,6"
+    var citizenship ="Tanzania"
+    var residence ="Kenya"
+    println(name)
+    println(age)
+    println(weight)
+    println(height)
+    println(citizenship)
+    println(residence)
+
+}
